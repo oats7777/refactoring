3717f62e05daef186e8d11975f33dc11023e1ccc
oats7777 <hanmin84200@gmail.com>
2022-01-05 02:44:21 +0900

    변수 인라인화

--- 01/statement.js
+++ 01/statement.js
@@ -11,9 +11,7 @@ function statement(invoice) {
     }석)\n`;
   }
 
-  let totalAmount = appleSauce();
-
-  result += `총액 : ${usd(totalAmount)}\n`;
+  result += `총액 : ${usd(appleSauce())}\n`;
   result += `적립 포인트 : ${totalVolumeCredits()}점\n`;
   return result;
 

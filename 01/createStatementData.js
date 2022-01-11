1bd8259ead14b4459fbd621560398445905919c0
oats7777 <hanmin84200@gmail.com>
2022-01-12 03:58:30 +0900

    play 클래스로 분리

--- 01/createStatementData.js
+++ 01/createStatementData.js
@@ -1,3 +1,10 @@
+class PerformanceCalculator {
+  constructor(aPerformance, aPlay) {
+    this.performance = aPerformance;
+    this.play = aPlay;
+  }
+}
+
 module.exports = function createStatementData(invoice, plays) {
   const result = {};
   result.customer = invoice.customer;
@@ -7,8 +14,12 @@ module.exports = function createStatementData(invoice, plays) {
   return result;
 
   function enrichPerformance(aPerformance) {
+    const calculator = new PerformanceCalculator(
+      aPerformance,
+      playFor(aPerformance)
+    );
     const result = Object.assign({}, aPerformance); // 얕은 복사 수행
-    result.play = playFor(result);
+    result.play = calculator.play;
     result.amount = amountFor(result);
     result.volumeCredits = volumeCreditsFor(result);
     return result;
